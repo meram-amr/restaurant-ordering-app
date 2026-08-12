67d20655d6ec02dbce4a03a1aa79a61d74745344
AhmedHassan-Ahmed <ahmedhassan8754321@gmail.com>
2026-08-12 15:25:50 +0300

    add login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
 import { Mail, Lock, ArrowRight, Leaf } from "lucide-react";
-import { redirect } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 const Field = ({
   label,
